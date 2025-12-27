a52a780abc5ea7fc87838a8f553fa4a30d3c8a1b
Snehal Patil <snehalpatil3627@gmail.com>
2025-12-27 16:53:03 +0530

    Add requirements.txt for Render deployment

--- Outpass Project/app.py
+++ Outpass Project/app.py
@@ -443,5 +443,5 @@ def update_status(req_id, status):
 # Run Flask App
 # -------------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
